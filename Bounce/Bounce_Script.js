493bf4b9d5227b81bebbf60ee5d2ad4ab5483ace
katherinesoconnor201 <52784564+katherinesoconnor201@users.noreply.github.com>
2020-07-02 21:29:10 -0400

    Add files via upload

--- Bounce/Bounce_Script.js
+++ Bounce/Bounce_Script.js
@@ -178,10 +178,14 @@ function clearIntro() {
 let d = [];
 window.addEventListener("touchstart", t_move);
 window.addEventListener("touchmove", t_move);
-window.addEventListener("touchmove", function(event) { event.preventDefault() });
 window.addEventListener("touchend", t_move);
 document.addEventListener("keydown", k_down);
 document.addEventListener("keyup", k_up);
+
+//Stop scrolling on ios...hopefully
+window.addEventListener("touchstart", function(event) { event.preventDefault() });
+window.addEventListener("touchmove", function(event) { event.preventDefault() });
+window.addEventListener("touchend", function(event) { event.preventDefault() });
 
 function t_move(event) {
     //player[0].x = event.touches[0].screenX - cvs.offsetLeft - player[0].width / 2;
